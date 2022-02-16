6d44019a44a4f91e060d2ea317888d3f19377994
Avi Flombaum <pgp@avi.nyc>
2022-02-16 10:30:20 -0500

    "Added sample theme"

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def sample
+    render layout: "sample-theme"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/sample-theme', to: "pages#sample"
 end
